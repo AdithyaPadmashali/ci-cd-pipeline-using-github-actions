f3b95199523ccbee1405741f41307f5f208278f3
naren951 <narendiran.work@gmail.com>
2021-06-23 13:03:45 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>HELLO WORLD</code>
+           <code>WELCOME TO MY WORLD</code>
         </p>
         <a
           className="App-link"
