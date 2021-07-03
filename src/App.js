dc60e42432bc90b6ec9da79a249a710fc49ed6aa
AdithyaPadmashali <57041819+AdithyaPadmashali@users.noreply.github.com>
2021-07-03 21:27:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          It works.
+          It works...
         </a>
         <p>
           Changing some stuff...
