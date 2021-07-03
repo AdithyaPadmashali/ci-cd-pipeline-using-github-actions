a34562a089cc7e132c7336a7872ada2294e6bfb7
AdithyaPadmashali <57041819+AdithyaPadmashali@users.noreply.github.com>
2021-07-03 21:18:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>WELCOME TO CCBD... testing</code>
+           <code>WELCOME TO CCBD</code>
         </p>
         <a
           className="App-link"
