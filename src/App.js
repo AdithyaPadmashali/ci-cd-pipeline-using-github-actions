bc0b270904d74eefc8865d9f8c077e0407606a7f
naren951 <narendiran.work@gmail.com>
2021-07-06 12:07:15 +0530

    Test from local repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>WELCOME TO CCBD</code>
+           <code>WELCOME TO NAREN</code>
         </p>
         <a
           className="App-link"
